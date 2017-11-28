fc444f8d927d37a0a0269e0b7880357c51644c0e
lyl <15852818791@163.com>
2017-11-28 22:59:40 +0800

    慢慢买-比价搜索

--- js/common/route.js
+++ js/common/route.js
@@ -1,27 +1,65 @@
-;(function( window ){
+;
+( function( window ) {
 
     var Route = {
         baseUrl: 'http://127.0.0.1:9090/api/',
 
         // 主页获取数据
         getindex: getindex,
+
+        // 获取分类标题
+        getcategorytitle: getcategorytitle,
+
+        // 获取分类列表
+        getcategory: getcategory,
     };
 
 
     /* 获取主页菜单栏数据 */
-    function getindex( url, callback ){
+    function getindex( url, callback ) {
         var url1 = Route.baseUrl + url;
-        $.ajax({
+        $.ajax( {
             type: 'get',
             url: url1,
             dataType: 'json',
-            success: function ( info ){
+            success: function( info ) {
                 callback && callback( info );
             }
-        });
+        } );
     }
+
+    // 获取分类标题
+    function getcategorytitle( callback ) {
+        var url1 = Route.baseUrl + 'getcategorytitle';
+        $.ajax( {
+            type: 'get',
+            url: url1,
+            dataType: 'json',
+            success: function( info ) {
+                callback && callback( info );
+            }
+        } );
+    }
+
+    // 获取分类列表
+    function getcategory( data, callback ) {
+        var url1 = Route.baseUrl + 'getcategory';
+        $.ajax( {
+            type: 'get',
+            url: url1,
+            data: {
+                titleid: data
+            },
+            dataType: 'json',
+            success: function( info ) {
+                callback && callback( info );
+            }
+        } );
+    }
+
+
 
 
     // 暴露给全局
     window.Route = Route;
-})( window );
+} )( window );
